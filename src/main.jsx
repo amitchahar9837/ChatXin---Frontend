2edda50cc3343208c7399e8f97a036895bd12049
amitchahar9837 <amitchahar9837@gmail.com>
2026-07-14 12:45:24 +0530

    add eruda for debugging

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,9 @@ import App from "./App.jsx";
 import { store } from "./redux/store.js";
 import "./index.css";
 import { VideoCallProvider } from "./components/chat/VideoCallContext.jsx";
+import eruda from "er";
 
+eruda.init();
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
